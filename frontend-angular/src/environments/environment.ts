f9305aff18c94849f0b7054f0ae70031ac42c8df
Douglas Santanna <d.santanna@retail-int.com>
2024-10-14 15:09:07 +0100

    update environment.ts

--- frontend-angular/src/environments/environment.ts
+++ frontend-angular/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://ms-learn-demo-api-management.azure-api.net"
+  apiUrl: "https://ms-learn-api-management-b5fqcpdca8gtcwe7.northeurope-01.azurewebsites.net"
 };
